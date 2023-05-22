0308e9bb72ac6e6c89256b2b5aa12b4831a790ca
oiab94AsusLaptop <oscar.alonso.994@gmail.com>
2023-05-22 14:50:58 -0400

    Sentencia select: Eliminar o añadir filas duplicadas

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 
 ## Index
 - [Sentencia select](/estructura-sentecia-select/select.sql)
+- [Sentencia select: Eliminar filas duplicadas](/estructura-sentecia-select/select-distintc.sql)
---
+++ estructura-sentecia-select/select-distintc.sql
@@ -0,0 +1,7 @@
+-- Para eliminar las filas duplicadas se utiliza la cláusula DISTINCT
+SELECT DISTINCT attribute_1
+FROM 						table_1
+
+-- Tambien podemos perdir explicitamente la inclusión de tuplas repetidas
+SELECT ALL 	attribute_1
+FROM 				table_1
